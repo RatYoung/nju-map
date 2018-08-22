b82a211515678b20706677f608650d1083b27cd1
RatYoung <yhrnju@qq.com>
2018-08-22 11:50:34 +0800

    first time

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  //调试模式开关，调试模式下只调用本地数据
+  debug: true,
+  //学校数据配置名称，学校英文缩写
+  school: "nju",
+  //高德路线规划密钥，必须加入https://restapi.amap.com为request合法域名
+  //密钥申请地址http://lbs.amap.com/api/javascript-api/summary/
+  key: "6c85f2ca2435fd125c3ac307885172b8", 
+    //图片CDN域名
+  imgCDN: ""
+}
